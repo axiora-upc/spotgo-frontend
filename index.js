81cc7e0babff7b109fd2d86644b86e26b2981b25
AdrixRyz <adrixryz@gmail.com>
2026-05-14 11:52:23 -0500

    fix: index.js

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ server.use(middlewares);
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  }),
+);
 
 server.use(router);
 
