187e4e9ddfbc192461c33ea24b936550c57af9b5
Johan Contreras <j.contreras.granados.04@gmail.com>
2026-05-14 03:03:21 -0500

    feat: enhance ConfirmDialog tests with additional providers and NoopAnimationsModule

--- src/app/shared/presentation/components/confirm-dialog/confirm-dialog.spec.ts
+++ src/app/shared/presentation/components/confirm-dialog/confirm-dialog.spec.ts
@@ -1,14 +1,42 @@
+/*
+  Imports tools to make testing Angular components easier.
+*/
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ConfirmDialog } from './confirm-dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
+/*
+  My confirm-dialog component, which is the one we want to test.
+*/
+import { ConfirmDialog, ConfirmDialogData } from './confirm-dialog';
+
+/*
+  describe groups all tests related to the ConfirmDialog component.
+*/
 describe('ConfirmDialog', () => {
   let component: ConfirmDialog;
   let fixture: ComponentFixture<ConfirmDialog>;
 
+  const dialogRefSpy = { close: () => undefined };
+  const dialogData: ConfirmDialogData = {
+    titleKey: 'demo.title',
+    messageKey: 'demo.message',
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ConfirmDialog],
+      imports: [ConfirmDialog, NoopAnimationsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: dialogData },
+        provideHttpClient(),
+        provideTranslateService({ fallbackLang: 'en', lang: 'en' }),
+        provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmDialog);
